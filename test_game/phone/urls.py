9545895a0e4fa420afb2b2564410a40dc3432c70
Alessandro Aloe <alessandroaloe@MacBookPro.wind3.hub>
2024-12-30 11:56:36 +0100

    Aggiunta dizionario per caratterostiche e specifiche e rimozione delle app inutili

--- test_game/phone/urls.py
+++ test_game/phone/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('phone/', views.phones, name='phone'),
-    path('phone/details/<int:id>', views.details, name='details'),
+    path('phone/details/<int:id>/', views.details, name='details'),
     path('phone/details/<int:id>/add_specific/', views.add_specific, name='add_specific'),
     path('add_phone/', views.add_phone, name='add_phone'),
 ]
